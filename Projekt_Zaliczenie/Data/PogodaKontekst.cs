9fa574a25482a81e0b5670e115356f98537b7e24
szymonst@student.agh.edu.pl <szymonst@student.agh.edu.pl>
2024-12-23 13:01:04 +0100

    scal

--- Projekt_Zaliczenie/Data/PogodaKontekst.cs
+++ Projekt_Zaliczenie/Data/PogodaKontekst.cs
@@ -1,13 +1,1 @@
-﻿using Microsoft.EntityFrameworkCore;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Projekt_Zaliczenie.Data
-{
-    public class PogodaKontekst : DbContext
-    {
-    }
-}
+﻿
